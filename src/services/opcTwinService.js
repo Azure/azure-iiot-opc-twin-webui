4c5ffff5ad793c0e7544e04afa1c4cb42c94d778
Marc Schier <marcschier@hotmail.com>
2018-10-05 09:30:26 +0200

    Make urls configurable

--- src/services/opcTwinService.js
+++ src/services/opcTwinService.js
@@ -1,12 +1,14 @@
 // Copyright (c) Microsoft. All rights reserved.
 
+import Config from 'app.config';
+
 import { Observable } from 'rxjs';
 import { HttpClient } from './httpClient';
 import { getItems } from 'utilities';
 import { toReadValueModel } from './models';
 
-const ENDPOINT_REGISTRY = 'http://localhost:9042/v1/';
-const ENDPOINT_OPC_TWIN = 'http://localhost:9041/v1/';
+const ENDPOINT_REGISTRY = Config.serviceUrls.registry || 'http://localhost:9042';
+const ENDPOINT_TWINS = Config.serviceUrls.twins || 'http://localhost:9041';
 
 /**
  * Contains methods for calling the device simulation microservice
@@ -17,39 +19,39 @@ export class OpcTwinService {
    * Returns a list of devicemodels
    */
   static getApplicationsList() {
-    const value = HttpClient.get(`${ENDPOINT_REGISTRY}Applications`, undefined, false)
-     .map(getItems);
-     return value;
+    const value = HttpClient.get(`${ENDPOINT_REGISTRY}/v1/Applications`, undefined, false)
+      .map(getItems);
+    return value;
   }
 
   static getApplication(id) {
-    return HttpClient.get(`${ENDPOINT_REGISTRY}Applications/${id}`, undefined, false);
+    return HttpClient.get(`${ENDPOINT_REGISTRY}/v1/Applications/${id}`, undefined, false);
   }
 
   static browseNode(endpointId, nodeId) {
     const queryString = nodeId ? `?nodeId=${encodeURIComponent(nodeId)}` : '';
-    return HttpClient.get(`${ENDPOINT_OPC_TWIN}Browse/${endpointId}${queryString}`, undefined, false)
+    return HttpClient.get(`${ENDPOINT_TWINS}/v1/Browse/${endpointId}${queryString}`, undefined, false)
       .map(({ node, references }) => ({
         node,
         references
-      }))
+      }));
   }
 
   static readNodeValue(endpointId, nodeId) {
     const queryString = nodeId ? `?nodeId=${encodeURIComponent(nodeId)}` : '';
-    return HttpClient.get(`${ENDPOINT_OPC_TWIN}Read/${endpointId}${queryString}`, undefined, false)
+    return HttpClient.get(`${ENDPOINT_TWINS}/v1/Read/${endpointId}${queryString}`, undefined, false)
       .map(toReadValueModel);
   }
 
   static writeNodeValue(endpointId, payload) {
-    return HttpClient.post(`${ENDPOINT_OPC_TWIN}Write/${endpointId}`, payload);
+    return HttpClient.post(`${ENDPOINT_TWINS}/v1/Write/${endpointId}`, payload);
   }
 
   static callNodeMethodMetadata(endpointId, payload) {
-    return HttpClient.post(`${ENDPOINT_OPC_TWIN}Call/${endpointId}/$metadata`, payload);
+    return HttpClient.post(`${ENDPOINT_TWINS}/v1/Call/${endpointId}/$metadata`, payload);
   }
 
   static callNodeMethod(endpointId, payload) {
-    return HttpClient.post(`${ENDPOINT_OPC_TWIN}Call/${endpointId}`, payload);
+    return HttpClient.post(`${ENDPOINT_TWINS}/v1/Call/${endpointId}`, payload);
   }
 }
