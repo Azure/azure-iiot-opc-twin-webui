3646fd619278c02eca2b07fe448d39ba832b0dcb
Daniele Colonna <dacol@microsoft.com>
2018-10-31 18:39:55 +0100

    change the treewiev open/close button. Ui fixes

--- src/utilities/svgs.js
+++ src/utilities/svgs.js
@@ -52,6 +52,8 @@ import TrashIconPath from 'assets/icons/trash.svg';
 import UploadIconPath from 'assets/icons/upload.svg';
 import WarningIconPath from 'assets/icons/warning.svg';
 import XIconPath from 'assets/icons/x.svg';
+import TreeArrowOpenIconPath from 'assets/icons/arrowOpen.svg';
+import TreeArrowCloseIconPath from 'assets/icons/arrowClose.svg';
 
 /** A helper object mapping svg names to their paths */
 export const svgs = {
@@ -110,5 +112,7 @@ export const svgs = {
   trash: TrashIconPath,
   upload: UploadIconPath,
   warning: WarningIconPath,
-  x: XIconPath
+  x: XIconPath,
+  TreeArrowClose: TreeArrowCloseIconPath,
+  TreeArrowOpen: TreeArrowOpenIconPath
 };
