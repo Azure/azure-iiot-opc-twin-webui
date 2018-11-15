29d8b575d69726d920cc87f087e4a080a9ba7ca5
Daniele Colonna <dacol@microsoft.com>
2018-11-15 14:15:21 +0100

    auth now use env variable. fIx ui style

--- src/app.config.js
+++ src/app.config.js
@@ -6,6 +6,10 @@ const Config = {
   maxRetryAttempts: 2,
   retryableStatusCodes: new Set([ 0, 502, 503 ]),
   defaultAjaxTimeout: 60000, // 10s
+
+  aadTenant: process.env.REACT_APP_AAD_TENANT,
+  aadAppId: process.env.REACT_APP_AAD_APP_ID,
+  aadInstance: process.env.REACT_APP_AAD_INSTANCE
 };
 
 export default Config;
